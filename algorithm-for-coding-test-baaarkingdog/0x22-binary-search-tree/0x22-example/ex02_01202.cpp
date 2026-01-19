d0d7b69f8ccb402d589ef7e9af1e1dc800fd330c
Luverduck <hyekko92@gmail.com>
2026-01-19 18:33:59 +0900

    0x16. 이진 검색 트리

--- algorithm-for-coding-test-baaarkingdog/0x22-binary-search-tree/0x22-example/ex02_01202.cpp
+++ algorithm-for-coding-test-baaarkingdog/0x22-binary-search-tree/0x22-example/ex02_01202.cpp
@@ -12,13 +12,14 @@ multiset<int> bags;
 
 void solution()
 {
-    // 가장 가격이 높은 보석부터 확인하며
-    // 해당 보석을 담을 수 있는 가방 중 최대 무게가 가장 작은 가방을 이용해 보석을 담는게 이득이다.
-
+    // 입력
     cin >> N >> K;
+    // 보석에 대한 입력
     for (int i = 0; i < N; ++i)
         cin >> jewel[i].second >> jewel[i].first;
+    // 보석을 가치 순으로 정렬
     sort(jewel, jewel + N);
+    // 가방에 대한 입력
     for (int i = 0; i < K; ++i)
     {
         int C;
@@ -26,16 +27,21 @@ void solution()
         bags.emplace(C);
     }
 
+    // 가치가 높은 보석부터 가방에 넣기
     long long res = 0;
     for (int i = N - 1; i >= 0; --i)
     {
         int M, V;
         tie(V, M) = jewel[i];
-        auto iter = lower_bound(bags.begin(), bags.end(), M);
+        // 최대 허용 무게가 M 이상인 가방 찾기
+        auto iter = bags.lower_bound(M);
         if (iter == bags.end()) continue;
+        // 보석의 전체 가치 갱신
         res += V;
+        // 보석을 담은 가방 제거
         bags.erase(iter);
     }
+    // 출력
     cout << res;
 }
 
